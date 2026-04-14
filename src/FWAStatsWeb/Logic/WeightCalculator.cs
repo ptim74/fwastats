19092136b5be01ea8fb0e6d9bbe2afbe7de8d6a4
ptim74 <38361503+ptim74@users.noreply.github.com>
2026-04-14 19:44:24 +0300

    Refactored estimated weight

--- src/FWAStatsWeb/Logic/WeightCalculator.cs
+++ src/FWAStatsWeb/Logic/WeightCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using FWAStatsWeb.Data;
+using FWAStatsWeb.Models.DataViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,14 +33,50 @@ public class WeightCalculator
         this.db = db;
     }
 
+    private static int MaxWeightForTH(int thLevel) => thLevel switch
+    {
+        <= 8  => Constants.MAXWEIGHT_TH8,
+        9     => Constants.MAXWEIGHT_TH9,
+        10    => Constants.MAXWEIGHT_TH10,
+        11    => Constants.MAXWEIGHT_TH11,
+        12    => Constants.MAXWEIGHT_TH12,
+        13    => Constants.MAXWEIGHT_TH13,
+        14    => Constants.MAXWEIGHT_TH14,
+        15    => Constants.MAXWEIGHT_TH15,
+        16    => Constants.MAXWEIGHT_TH16,
+        17    => Constants.MAXWEIGHT_TH17,
+        _     => Constants.MAXWEIGHT_TH18,
+    };
+
     public IEnumerable<Results> Calculate()
     {
-        var qthlevelq = (from m in db.Members
-                 join p in db.Players on m.Tag equals p.Tag
-                 group m by new { m.ClanTag, p.TownHallLevel } into g
-                 select new { g.Key.ClanTag, g.Key.TownHallLevel, Count = g.Count() }).ToLookup(t => t.ClanTag);
+        // Subquery: latest war ID per clan, then join its members and outer join weights
+        var latestWarIds = db.Wars
+            .GroupBy(w => w.ClanTag)
+            .Select(g => new { ClanTag = g.Key, WarID = g.Max(w => w.ID) });
 
-        foreach(var thlevels in qthlevelq)
+        var warData = (from lw in latestWarIds
+                       join wm in db.WarMembers on lw.WarID equals wm.WarID
+                       where !wm.IsOpponent
+                       join wt in db.Weights on wm.Tag equals wt.Tag into wtGroup
+                       from wt in wtGroup.DefaultIfEmpty()
+                       select new
+                       {
+                           lw.ClanTag,
+                           MemberTag = wm.Tag,
+                           wm.MapPosition,
+                           wm.TownHallLevel,
+                           WarWeight = wt != null ? wt.WarWeight : 0
+                       })
+                      .ToLookup(x => x.ClanTag);
+
+        var qthlevelq = (from m in db.Members
+                         join p in db.Players on m.Tag equals p.Tag
+                         group m by new { m.ClanTag, p.TownHallLevel } into g
+                         select new { g.Key.ClanTag, g.Key.TownHallLevel, Count = g.Count() })
+                        .ToLookup(t => t.ClanTag);
+
+        foreach (var thlevels in qthlevelq)
         {
             var ret = new Results { Tag = thlevels.Key };
 
@@ -47,135 +84,55 @@ public class WeightCalculator
             {
                 switch (th.TownHallLevel)
                 {
-                    case 18:
-                        ret.Th18Count = th.Count;
-                        break;
-                    case 17:
-                        ret.Th17Count = th.Count;
-                        break;
-                    case 16:
-                        ret.Th16Count = th.Count;
-                        break;
-                    case 15:
-                        ret.Th15Count = th.Count;
-                        break;
-                    case 14:
-                        ret.Th14Count = th.Count;
-                        break;
-                    case 13:
-                        ret.Th13Count = th.Count;
-                        break;
-                    case 12:
-                        ret.Th12Count = th.Count;
-                        break;
-                    case 11:
-                        ret.Th11Count = th.Count;
-                        break;
-                    case 10:
-                        ret.Th10Count = th.Count;
-                        break;
-                    case 9:
-                        ret.Th9Count = th.Count;
-                        break;
-                    case 8:
-                        ret.Th8Count = th.Count;
-                        break;
-                    default:
-                        ret.ThLowCount += th.Count;
-                        break;
+                    case 18: ret.Th18Count = th.Count; break;
+                    case 17: ret.Th17Count = th.Count; break;
+                    case 16: ret.Th16Count = th.Count; break;
+                    case 15: ret.Th15Count = th.Count; break;
+                    case 14: ret.Th14Count = th.Count; break;
+                    case 13: ret.Th13Count = th.Count; break;
+                    case 12: ret.Th12Count = th.Count; break;
+                    case 11: ret.Th11Count = th.Count; break;
+                    case 10: ret.Th10Count = th.Count; break;
+                    case 9:  ret.Th9Count = th.Count; break;
+                    case 8:  ret.Th8Count = th.Count; break;
+                    default: ret.ThLowCount += th.Count; break;
                 }
             }
 
-            //Calculate estimated max weight
-            var availableMembers = Constants.WAR_SIZE1; //TODO
+            var clanData = warData[thlevels.Key];
+            if (clanData.Any())
+            {
+                // Members are ordered top-to-bottom (position 1 = highest weight),
+                // so iterate bottom-to-top and carry the last known weight upward
+                // to fill in any missing (0) or outdated (lower-than-below) entries.
+                var members = clanData
+                    .OrderByDescending(m => m.MapPosition)
+                    .ToList();
 
-            var thLevelMembers = ret.Th18Count < availableMembers ? ret.Th18Count : availableMembers;
-            var maxWeight = thLevelMembers * Constants.MAXWEIGHT_TH18 / 1000 - 5;
-            availableMembers -= thLevelMembers;
+                int lastWeight = 0;
+                int totalWeight = 0;
 
-            thLevelMembers = ret.Th17Count < availableMembers ? ret.Th17Count : availableMembers;
-            maxWeight += thLevelMembers * Constants.MAXWEIGHT_TH17 / 1000 - 5;
-            availableMembers -= thLevelMembers;
+                foreach (var member in members)
+                {
+                    var maxWeight = MaxWeightForTH(member.TownHallLevel);
+                    int weight = member.WarWeight == 0
+                        ? maxWeight - 5000 // defaults to almost maxed weight for the TH level
+                        : member.WarWeight;
 
-            thLevelMembers = ret.Th16Count < availableMembers ? ret.Th16Count : availableMembers;
-            maxWeight += thLevelMembers * Constants.MAXWEIGHT_TH16 / 1000 - 5;
-            availableMembers -= thLevelMembers;
+                    // Carry up from below, but cap to current TH's maximum
+                    lastWeight = weight = Math.Max(weight, Math.Min(lastWeight, maxWeight));
 
-            thLevelMembers = ret.Th15Count < availableMembers ? ret.Th15Count : availableMembers;
-            maxWeight += thLevelMembers * Constants.MAXWEIGHT_TH15 / 1000 - 5;
-            availableMembers -= thLevelMembers;
+                    totalWeight += weight;
+                }
 
-            thLevelMembers = ret.Th14Count < availableMembers ? ret.Th14Count : availableMembers;
-            maxWeight += thLevelMembers * Constants.MAXWEIGHT_TH14 / 1000 - 5;
-            availableMembers -= thLevelMembers;
+                // Normalize to a 50-member war so weights are comparable across different war sizes
+                if (members.Count > 0)
+                    totalWeight = totalWeight * Constants.WAR_SIZE2 / members.Count;
 
-            thLevelMembers = ret.Th13Count < availableMembers ? ret.Th13Count : availableMembers;
-            maxWeight += thLevelMembers * Constants.MAXWEIGHT_TH13 / 1000 - 5;
-            availableMembers -= thLevelMembers;
-
-            thLevelMembers = ret.Th12Count < availableMembers ? ret.Th12Count : availableMembers;
-            maxWeight += thLevelMembers * Constants.MAXWEIGHT_TH12 / 1000 - 5;
-            availableMembers -= thLevelMembers;
-
-            thLevelMembers = ret.Th11Count < availableMembers ? ret.Th11Count : availableMembers;
-            maxWeight += thLevelMembers * Constants.MAXWEIGHT_TH11 / 1000 - 5;
-            availableMembers -= thLevelMembers;
-
-            thLevelMembers = ret.Th10Count < availableMembers ? ret.Th10Count : availableMembers;
-            maxWeight += thLevelMembers * Constants.MAXWEIGHT_TH10 / 1000 - 5;
-            availableMembers -= thLevelMembers;
-
-            thLevelMembers = ret.Th9Count < availableMembers ? ret.Th9Count : availableMembers;
-            maxWeight += thLevelMembers * Constants.MAXWEIGHT_TH9 / 1000 - 5;
-            availableMembers -= thLevelMembers;
-
-            thLevelMembers = ret.Th8Count < availableMembers ? ret.Th8Count : availableMembers;
-            maxWeight += thLevelMembers * Constants.MAXWEIGHT_TH8 / 1000 - 5;
-            availableMembers -= thLevelMembers;
-
-
-            //Calculate estimated min weight
-            availableMembers = Constants.WAR_SIZE1; //TODO
-            thLevelMembers = ret.Th8Count < availableMembers ? ret.Th8Count : availableMembers;
-
-            //TH8 count has limits
-            if (thLevelMembers > 5)
-                thLevelMembers = 5;
-            if (thLevelMembers > 1 && maxWeight > 3300)
-                thLevelMembers = 1;
-            var th8Members = thLevelMembers;
-
-            var minWeight = thLevelMembers * Constants.MAXWEIGHT_TH8 / 1000 - 5;
-            availableMembers -= thLevelMembers;
-
-            thLevelMembers = ret.Th9Count < availableMembers ? ret.Th9Count : availableMembers;
-            minWeight += thLevelMembers * Constants.MAXWEIGHT_TH9 / 1000 - 5;
-            availableMembers -= thLevelMembers;
-
-            thLevelMembers = ret.Th10Count < availableMembers ? ret.Th10Count : availableMembers;
-            minWeight += thLevelMembers * Constants.MAXWEIGHT_TH10 / 1000 - 5;
-            availableMembers -= thLevelMembers;
-
-            thLevelMembers = ret.Th11Count < availableMembers ? ret.Th11Count : availableMembers;
-            minWeight += thLevelMembers * Constants.MAXWEIGHT_TH11 / 1000 - 5;
-            availableMembers -= thLevelMembers;
-
-            thLevelMembers = ret.Th12Count < availableMembers ? ret.Th12Count : availableMembers;
-            minWeight += thLevelMembers * Constants.MAXWEIGHT_TH12 / 1000 - 5;
-            availableMembers -= thLevelMembers;
-
-            thLevelMembers = ret.Th13Count < availableMembers ? ret.Th13Count : availableMembers;
-            minWeight += thLevelMembers * Constants.MAXWEIGHT_TH13 / 1000 - 5;
-            availableMembers -= thLevelMembers;
-
-            thLevelMembers = ret.Th14Count < availableMembers ? ret.Th14Count : availableMembers;
-            minWeight += thLevelMembers * Constants.MAXWEIGHT_TH14 / 1000 - 5;
-            availableMembers -= thLevelMembers;
-
-            ret.EstimatedWeight = (maxWeight + minWeight) / 2;
+                ret.EstimatedWeight = totalWeight / 1000;
+            }
 
             yield return ret;
         }
-
     }
 }
